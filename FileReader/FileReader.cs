ea847ee9762956385756bf265d758e15997dd33a
miroslav-slavchev <miroslav.svetoslavov.slavchev@gmail.com>
2022-05-25 20:38:51 +0300

    Resolve PR comments.

--- FileReader/FileReader.cs
+++ FileReader/FileReader.cs
@@ -7,17 +7,30 @@ using System.Threading.Tasks;
 
 namespace FileReader
 {
-    public static class FileReader
+    public static class TestDataFileReader
     {
+        public static string DirectoryName { get; set; } = "TestData";
+
         public static string GetFileTextContent(string fileName)
         {
-            string relativePath = $"\\TestData";
-            string baseDirPath = AppDomain.CurrentDomain.BaseDirectory;
-            baseDirPath = baseDirPath.Replace("\\bin\\Debug\\net5.0\\", "");
-            string directoryPath = baseDirPath + relativePath;
-            string filePath = $"\\{directoryPath}\\{fileName}";
+            string filePath = GetFilePath(fileName);
             string textContent = File.ReadAllText(filePath);
             return textContent;
+        }
+
+        private static string GetFilePath(string fileName)
+        {
+            string filePath = $"\\{GetDirectoryPath()}\\{fileName}";
+            return filePath;
+        }
+
+        private static string GetDirectoryPath()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(AppDomain.CurrentDomain.BaseDirectory);
+            stringBuilder.Replace("\\bin\\Debug\\net5.0\\", "");
+            stringBuilder.Append($"\\{DirectoryName}");
+            return stringBuilder.ToString();
         }
     }
 }
