e71be593439bd05e44282b1b1befd00672221893
miroslav-slavchev <miroslav.svetoslavov.slavchev@gmail.com>
2022-05-25 20:17:20 +0300

    Implement file reader

---
+++ FileReader/FileReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    public static class FileReader
+    {
+        public static string GetFileTextContent(string fileName)
+        {
+            string relativePath = $"\\TestData";
+            string baseDirPath = AppDomain.CurrentDomain.BaseDirectory;
+            baseDirPath = baseDirPath.Replace("\\bin\\Debug\\net5.0\\", "");
+            string directoryPath = baseDirPath + relativePath;
+            string filePath = $"\\{directoryPath}\\{fileName}";
+            string textContent = File.ReadAllText(filePath);
+            return textContent;
+        }
+    }
+}
