1eb8c324c271e03b7d880b85093c3e4c5d6bdaad
miroslav-slavchev <74956380+miroslav-slavchev@users.noreply.github.com>
2022-05-25 20:01:06 +0300

    Update Program.cs
    Add second message.

--- FileReader/Program.cs
+++ FileReader/Program.cs
@@ -7,6 +7,7 @@ namespace FileReader
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello User!");
         }
     }
 }
